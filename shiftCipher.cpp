c681f0ccd35bbfbd330b0f718ab1c0e1bf77e832
Khushbu Jain <149000531+khushbujain41709@users.noreply.github.com>
2026-02-11 16:48:58 +0530

    Implemented shift cipher for encryption and decryption

---
+++ shiftCipher.cpp
@@ -0,0 +1,39 @@
+// Shift Cipher
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    string s;
+    cout<<"Enter any capital string : ";
+    cin>>s;
+    // A - 0, Z - 25
+    string plain = "";
+    string cipher = "";
+    int key;
+    cout<<"Enter value of key between 0 to 25 : ";
+    cin>>key;
+    int encrypt = 0;
+    int decrypt = 0;
+    for(int i = 0; i<s.size(); i++){
+        if(s[i] == ' ') continue;
+        encrypt = ((s[i] - 'A') + key) % 26;
+        if(encrypt >= 0){
+            plain += (char)encrypt + 'A';
+        }
+        else{
+            plain += (char)(26 + encrypt)  + 'A';
+        }
+    }
+    for(int i = 0; i<s.size(); i++){
+        decrypt = ((plain[i] - 'A') - key) % 26;
+        if(decrypt >= 0){
+            cipher += (char)decrypt + 'A';
+        }
+        else{
+            cipher += (char)(26 + decrypt)  + 'A';
+        }
+    }
+    cout<<"Encrypted code is : "<<plain<<endl;
+    cout<<"Decrypted code is : "<<cipher<<endl;
+    return 0;
+}
